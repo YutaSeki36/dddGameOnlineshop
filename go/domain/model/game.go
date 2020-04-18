2db8e9015b59348bd3fcd534a3bba7c4c9e1d871
YutaSeki36 <sekiyuta0306@gmail.com>
2020-04-18 17:09:45 +0900

    リファクタリング

--- go/domain/model/game.go
+++ go/domain/model/game.go
@@ -8,21 +8,21 @@ import (
 
 // Game ゲームドメインモデル
 type Game struct {
-	id          int
-	title       values.Title
-	detail      string
-	genre       string
-	price       values.Price
-	releaseDate time.Time
-	maker       string
+	ID          int
+	Title       values.Title
+	Detail      string
+	Genre       string
+	Price       values.Price
+	ReleaseDate time.Time
+	Maker       string
 }
 
 // GemeInput Game生成用のインプット構造体
 type GemeInput struct {
-	title       string
-	detail      string
-	genre       string
-	price       int
-	releaseDate time.Time
-	maker       string
+	Title       string
+	Detail      string
+	Genre       string
+	Price       int
+	ReleaseDate time.Time
+	Maker       string
 }
