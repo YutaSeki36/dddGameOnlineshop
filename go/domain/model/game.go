0c14a6efe4768296906baeb463bcd8231eaeb4bb
YutaSeki36 <sekiyuta0306@gmail.com>
2020-04-19 17:53:30 +0900

    Gameモデル生成メソッド追加

--- go/domain/model/game.go
+++ go/domain/model/game.go
@@ -1,25 +1,28 @@
 package model
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/dddGameOnlineshop/go/domain/values"
 )
 
 // Game ゲームドメインモデル
+// フィールドの頭文字を小文字にして，不用意にGetされないようにする
+// 必要な項目はGetterを用意する
 type Game struct {
-	ID          int
-	Title       values.Title
-	Detail      string
-	Genre       string
-	Price       values.Price
-	ReleaseDate time.Time
-	Maker       string
-	Cero        values.Cero
+	id          int
+	title       values.Title
+	detail      string
+	genre       string
+	price       values.Price
+	releaseDate time.Time
+	maker       string
+	cero        values.Cero
 }
 
-// GemeInput Game生成用のインプット構造体
-type GemeInput struct {
+// GameInput Game生成用のインプット構造体
+type GameInput struct {
 	Title       string
 	Detail      string
 	Genre       string
@@ -27,4 +30,32 @@ type GemeInput struct {
 	ReleaseDate time.Time
 	Maker       string
 	Cero        string
+}
+
+// NewGame Gameオブジェクトの生成メソッド
+func NewGame(input GameInput) (*Game, error) {
+	title, err := values.NewTitle(input.Title)
+	if err != nil {
+		return nil, fmt.Errorf("タイトル生成に失敗しました．エラー内容: %s", err.Error())
+	}
+
+	price, err := values.NewPrice(input.Price)
+	if err != nil {
+		return nil, fmt.Errorf("価格生成に失敗しました．エラー内容: %s", err.Error())
+	}
+
+	cero, err := values.NewCero(input.Cero)
+	if err != nil {
+		return nil, fmt.Errorf("Ceroオブジェクト生成に失敗しました．エラー内容: %s", err.Error())
+	}
+
+	return &Game{
+		title:       *title,
+		detail:      input.Detail,
+		genre:       input.Genre,
+		price:       *price,
+		releaseDate: input.ReleaseDate,
+		maker:       input.Maker,
+		cero:        *cero,
+	}, nil
 }
