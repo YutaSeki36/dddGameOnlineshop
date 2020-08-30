06377875eac7aaa8398801fe2d70b50e0eb33238
YutaSeki36 <sekiyuta0306@gmail.com>
2020-08-30 17:43:55 +0900

    Dockerで動作環境を構築

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/dddGameOnlineshop
-
-go 1.14
-
-require github.com/dustin/go-humanize v1.0.0
--- go/main.go
+++ go/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
+// Docker動作検証用にHello Worldを返すサーバを立てる
 func main() {
-	fmt.Println("Hello DDD")
+	http.HandleFunc("/", HelloHandler)
+	http.ListenAndServe(":8888", nil)
+}
+
+// HelloHandler サーバーの処理内容を記述 --- (*3)
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
 }
